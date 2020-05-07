568b07d479d72bf84fcada4cb4d3eb25229617c5
miketheo423 <miketheo423@gmail.com>
2020-05-06 18:46:41 -0600

    adds table loader component

---
+++ src/components/Table/TableLoader/TableLoader.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import './TableLoader.scss';
+
+const TableLoader: FC = () => {
+  const renderRows = (rows: number, columns: number) => {
+    return [...Array(rows).keys()].map((key) => {
+      return (
+        <div key={key} className='table-loader__row'>
+          {renderColumns(columns)}
+        </div>
+      );
+    });
+  };
+  const renderColumns = (columns: number) => {
+    return [...Array(columns).keys()].map((key) => <span key={key} className='line'></span>);
+  };
+
+  return (
+    <section className='table-loader'>
+      <div className='table-loader__header table-loader__row'>{renderColumns(5)}</div>
+      <div className='table-loader__body'>{renderRows(8, 5)}</div>
+    </section>
+  );
+};
+
+export default TableLoader;
