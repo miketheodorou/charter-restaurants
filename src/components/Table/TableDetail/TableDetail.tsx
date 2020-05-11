11897b13e84d5730d1921a2d3b0e5bccba6b00f9
miketheo423 <miketheo423@gmail.com>
2020-05-10 20:25:16 -0600

    adds map view to table detail

--- src/components/Table/TableDetail/TableDetail.tsx
+++ src/components/Table/TableDetail/TableDetail.tsx
@@ -20,7 +20,7 @@ interface Address {
 }
 
 const TableDetail: FC<Props> = ({ restaurant, expanded }: Props) => {
-  const { address1: address, city, state, zip, tags, website, hours } = restaurant;
+  const { address1: address, city, state, zip, tags, website, hours, lat, long } = restaurant;
 
   const renderAddress = ({ address, city, state, zip }: Address) => {
     return `${address}, ${city}, ${state} ${zip}`;
@@ -42,47 +42,55 @@ const TableDetail: FC<Props> = ({ restaurant, expanded }: Props) => {
 
   return (
     <td className='detail-view' colSpan={5}>
-      <div className='section website'>
-        <h3 className='section__title'>
-          <MonitorIcon />
-          Website
-        </h3>
-        <a
-          tabIndex={expanded ? 0 : -1}
-          href={website}
-          title='Link to restaurant website'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='btn btn--tertiary'
-          onClick={(e) => e.stopPropagation()}
-        >
-          Visit Website
-        </a>
+      <div className='grid'>
+        <div className='section website'>
+          <h3 className='section__title'>
+            <MonitorIcon />
+            Website
+          </h3>
+          <a
+            tabIndex={expanded ? 0 : -1}
+            href={website}
+            title='Link to restaurant website'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='btn btn--tertiary'
+            onClick={(e) => e.stopPropagation()}
+          >
+            Visit Website
+          </a>
+        </div>
+
+        <div className='section tags'>
+          <h3 className='section__title'>
+            <TagsIcon />
+            Tags
+          </h3>
+          <p className='section__text'>{cleanAndRenderTags(tags)}</p>
+        </div>
+
+        <div className='section address'>
+          <h3 className='section__title'>
+            <MarkerIcon />
+            Address
+          </h3>
+          <p className='section__text'>{renderAddress({ address, city, state, zip })}</p>
+        </div>
+
+        <div className='section hours'>
+          <h3 className='section__title'>
+            <HoursIcon />
+            Hours
+          </h3>
+          {renderHours(hours)}
+        </div>
       </div>
 
-      <div className='section tags'>
-        <h3 className='section__title'>
-          <TagsIcon />
-          Tags
-        </h3>
-        <p className='section__text'>{cleanAndRenderTags(tags)}</p>
-      </div>
-
-      <div className='section address'>
-        <h3 className='section__title'>
-          <MarkerIcon />
-          Address
-        </h3>
-        <p className='section__text'>{renderAddress({ address, city, state, zip })}</p>
-      </div>
-
-      <div className='section hours'>
-        <h3 className='section__title'>
-          <HoursIcon />
-          Hours
-        </h3>
-        {renderHours(hours)}
-      </div>
+      <iframe
+        className='map-view'
+        title='Retaurant Location Map'
+        src={`https://maps.google.com/maps?q=${lat},${long}&hl=es;z=14&zoom=15&output=embed`}
+      ></iframe>
     </td>
   );
 };
